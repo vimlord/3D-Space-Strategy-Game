df5626dc5d8c4d19424d8ed26a165759ec70fa45
Christopher <chrishittner@gmail.com>
2014-03-11 14:18:10 -0400

    Create CycleRunner.java
    CycleRunner runs cycles in the game's calculations.

---
+++ main/CycleRunner.java
@@ -0,0 +1,19 @@
+package main;
+/*
+ * Runs the necessary code to execute a cycle in the game's processes.
+ */
+ 
+/**
+ *
+ * @author Christopher Hittner
+ */
+public class CycleRunner {
+    public static final int cyclesPerSecond = 1000;
+     
+     
+    public static void executeCycle(){
+        EntityList.executeGravity();
+        EntityList.executeMovement();
+    }
+}
+
