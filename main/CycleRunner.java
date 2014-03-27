90bcebf78eb0a44be96b9bcfd4781963a5cb681c
Christopher <chrishittner@gmail.com>
2014-03-27 13:50:50 -0400

    Moved Code
    I moved executeMovement() below executeCollisions() to stop potential problems.

--- main/CycleRunner.java
+++ main/CycleRunner.java
@@ -8,13 +8,14 @@ package main;
  * @author Christopher Hittner
  */
 public class CycleRunner {
+    //Number of times the exexuteCycle method is assumed to run each second.
     public static final int cyclesPerSecond = 1000;
      
      
     public static void executeCycle(){
         EntityList.executeGravity();
-        EntityList.executeMovement();
         EntityList.executeCollisions();
+        EntityList.executeMovement();
     }
 }
 
