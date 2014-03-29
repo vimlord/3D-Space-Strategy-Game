354375e3d5577ca671ca36c0e79e8ae79149ca0b
Christopher <chrishittner@gmail.com>
2014-03-29 10:53:09 -0400

    Created ProjectileLauncher Interface
    This is done so that there can be a similarity between different
    Projectile launchers, as each will be too different for subclasses to be
    used.

--- entities/projectiles/Railgun.java
+++ entities/projectiles/Railgun.java
@@ -5,6 +5,7 @@
 
 package entities.projectiles;
 
+import entities.*;
 import entities.ships.*;
 import main.CycleRunner;
 import main.EntityList;
@@ -13,7 +14,7 @@ import main.EntityList;
  *
  * @author Christopher Hittner
  */
-public class Railgun {
+public class Railgun implements ProjectileLauncher{
     public final int cooldownTime = 60 * CycleRunner.cyclesPerSecond;
     private int cooldown = 0;
     private int ammo;
@@ -47,8 +48,33 @@ public class Railgun {
         }
     }
     
+    /**
+     * Fires a slug without checking direction that the shooter is facing
+     * @param shooter The Ship that is shooting
+     */
     public void fire(Ship shooter){
         if(canFire()){
+            EntityList.addProjectile(new Slug(shooter));
+            cooldown = cooldownTime;
+            ammo--;
+        }
+    }
+    /**
+     * Fires a slug only if the shooter is aiming at a target
+     * @param shooter
+     * @param target
+     */
+    public void fire(Ship shooter, Entity target){
+        double relX = shooter.getX() - target.getX();
+        double relY = shooter.getY() - target.getY();
+        double relZ = shooter.getZ() - target.getZ();
+        double relCoord = Math.sqrt(Math.pow(relX,2) + Math.pow(relY,2) + Math.pow(relZ,2));
+        
+        double relAngleX = Math.cos(shooter.getXZ_ROT()) * Math.cos(shooter.getY_ROT());
+        double relAngleY = Math.sin(shooter.getY_ROT());
+        double relAngleZ = Math.sin(shooter.getXZ_ROT()) * Math.cos(shooter.getY_ROT());
+        
+        if(canFire() && ((relX/relCoord == relAngleX) && (relY/relCoord == relAngleY) && (relZ/relCoord == relAngleZ))){
             EntityList.addProjectile(new Slug(shooter));
             cooldown = cooldownTime;
             ammo--;
