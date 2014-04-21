034d1beef06afa2160e94c9049c7a843bf32f834
Christopher <chrishittner@gmail.com>
2014-04-21 15:52:13 -0500

    Added Planet Class
    The Planet class holds data for a planet. I ended up removing a lot of
    code from the CelestialBody class and transferring it to the Planet
    class because it had more to do with planets than celestial bodies in
    general.

--- entities/celestialBodies/CelestialBody.java
+++ entities/celestialBodies/CelestialBody.java
@@ -14,9 +14,7 @@ import main.CycleRunner;
  * @author Christopher Hittner
  */
 public class CelestialBody extends Entity{
-    private double atmPressure = 0;
-    private boolean hasAtmosphere;
-    private double atmosphereHeight = 0;
+    
      
     /**
      * @param X The x-coordinate
@@ -27,129 +25,6 @@ public class CelestialBody extends Entity{
      */
     public CelestialBody(double X, double Y, double Z, double M, double R){
         super(X, Y, Z, M, R);
-        hasAtmosphere = false;
-    }
-     
-    /**
-     *
-     * @param X The x-coordinate
-     * @param Y The x-coordinate
-     * @param Z The x-coordinate
-     * @param M The mass
-     * @param R The radius
-     * @param pressure The pressure at the surface
-     * @param height The atmosphere's height
-     */
-    public CelestialBody(double X, double Y, double Z, double M, double R, double pressure, double height){
-        super(X, Y, Z, M, R);
-        atmPressure = pressure;
-        if(height <= 0){
-            atmosphereHeight = 0;
-            hasAtmosphere = false;
-        } else {
-            atmosphereHeight = height;
-            hasAtmosphere = true;
-        }
-    }
-     
-    /**
-     *
-     * @param other The air pressure wherever the Entity is
-     * @return The air pressure at the Entity's coordinates
-     */
-    public double getPressure(Entity other){
-        if(testCollision(other) || !hasAtmosphere){
-            return 0;
-        }
-        double distanceX = other.getX() - this.x;
-        double distanceY = other.getY() - this.y;
-        double distanceZ = other.getZ() - this.z;
-        double distance = Math.sqrt(Math.pow(distanceX,2) + Math.pow(distanceY,2) + Math.pow(distanceZ,2));
-        distance -= this.radius;
-        if(distance <= atmosphereHeight)
-            return (atmPressure * (1-(double)(distance/atmosphereHeight)));
-        else return 0;
-    }
-     
-    /**
-     *
-     * @param X The x-coordinate
-     * @param Y The y-coordinate
-     * @param Z The z-coordinate
-     * @return The air pressure at the provided coordinates
-     */
-    public double getPressure(double X, double Y, double Z){
-        if(!hasAtmosphere){
-            return 0;
-        }
-        double distanceX = X - this.x;
-        double distanceY = Y - this.y;
-        double distanceZ = Y - this.z;
-        double distance = Math.sqrt(Math.pow(distanceX,2) + Math.pow(distanceY,2) + Math.pow(distanceZ,2));
-        distance -= this.radius;
-        if(distance < 0 || distance > atmosphereHeight){
-            return 0;
-        } else
-            return (atmPressure * (1-(double)(distance/atmosphereHeight)));
-    }
-    
-    public boolean testCollision(Entity other){
-        double distanceX = other.getX() - this.x;
-        double distanceY = other.getY() - this.y;
-        double distanceZ = other.getZ() - this.z;
-        double distance = Math.sqrt(Math.pow(distanceX,2) + Math.pow(distanceY,2) + Math.pow(distanceZ,2));
-        
-        if((this.radius + atmosphereHeight) + other.getRadius() >= distance){
-            return true;
-        }
-        return false;
-    }
-    
-    /**
-     * Performs collision physics with an Entity object
-     * @param other The Entity being collided with
-     */
-    public void collide(Entity other){
-        //If the objects are colliding physically, it will deal with the objects as such
-        if(super.testCollision(other)){
-            super.collide(other);
-        }
-        if(hasAtmosphere){
-            double relVelX = other.getSpeedX() - this.velX;
-            double relVelY = other.getSpeedY() - this.velY;
-            double relVelZ = other.getSpeedZ() - this.velZ;
-            double relVel = Math.sqrt(Math.pow(relVelX,2) + Math.pow(relVelY,2) + Math.pow(relVelZ,2));
-
-            double dragForce = getPressure(other) * 0.47 * Math.pow(relVel,2);
-            other.setSpeedX(other.getSpeedX() - ((relVelX/relVel) * dragForce/other.getMass()));
-            other.setSpeedY(other.getSpeedY() - ((relVelY/relVel) * dragForce/other.getMass()));
-            other.setSpeedZ(other.getSpeedZ() - ((relVelZ/relVel) * dragForce/other.getMass()));
-        }
-        
-    }
-    
-    /**
-     * Performs collision physics with a Ship object
-     * @param other The ship
-     */
-    public void collide(Ship other){
-        //If the objects are colliding physically, it will deal with the objects as such
-        if(super.testCollision(other)){
-            other.collide(this);
-        }
-        if(hasAtmosphere){
-            double relVelX = other.getSpeedX() - this.velX;
-            double relVelY = other.getSpeedY() - this.velY;
-            double relVelZ = other.getSpeedZ() - this.velZ;
-            double relVel = Math.sqrt(Math.pow(relVelX,2) + Math.pow(relVelY,2) + Math.pow(relVelZ,2));
-
-            double dragForce = getPressure(other) * 0.47 * Math.pow(relVel,2);
-            other.setSpeedX(other.getSpeedX() - ((relVelX/relVel) * dragForce/other.getMass()));
-            other.setSpeedY(other.getSpeedY() - ((relVelY/relVel) * dragForce/other.getMass()));
-            other.setSpeedZ(other.getSpeedZ() - ((relVelZ/relVel) * dragForce/other.getMass()));
-
-            other.damage(getPressure(other) * (relVel - 120)/CycleRunner.cyclesPerSecond);
-        }
     }
      
 }
