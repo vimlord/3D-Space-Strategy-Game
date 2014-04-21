8506ceaeb9f96fe4a8f9eaa63db96352c49b0a73
Christopher <chrishittner@gmail.com>
2014-04-21 17:01:52 -0500

    Added Code For Orbit
    The code I added will calculate a circular orbit around an Entity. My
    initial intention was to have it calculate an elliptical orbit, but it
    requires calculus, which I have not yet learned. I will try to write
    code for elliptical orbits, but it may take time.

--- physics/Orbit.java
+++ physics/Orbit.java
@@ -1,13 +1,79 @@
 /*
- * The Orbit class stores data for an orbit around a CelestialBody object.
+ * The Orbit class stores data for a circular orbit around a CelestialBody object.
+ * All numbers are relative to the body being orbited, not the origin (0,0,0).
  */
 
 package physics;
+
+import entities.Entity;
 
 /**
  *
  * @author Christopher
  */
-public class Orbit {
+public class Orbit implements PhysicsConstants{
+    protected double a, inc, LAN, MaE, mass;
+    
+    protected double velX, velY, velZ;
+    protected double x, y, z;
+    
+    /**
+     * Defines an orbit around an Entity
+     * @param eccentricity measured in radians
+     * @param semimajorAxis
+     * @param inclination
+     * @param longitudeAscendingNode
+     * @param argumentPeriapsis
+     * @param meanAnomaly
+     * @param entity The Entity being orbited
+     */
+    public Orbit(double semimajorAxis, double inclination, double longitudeAscendingNode, double meanAnomaly, Entity entity){
+        a = semimajorAxis;
+        inc = inclination;
+        LAN = longitudeAscendingNode;
+        MaE = meanAnomaly;
+        mass = entity.getMass();
+        
+        calculateData();
+    }
+
+    private void calculateData() {
+        double vel = Math.sqrt(G * mass/a);
+        
+        velY = vel * Math.cos(MaE) * Math.sin(inc);
+        y = a * Math.cos(MaE) * Math.sin(inc);
+        
+        double velXZ = vel * Math.sin(MaE) * Math.cos(inc);
+        double xz = Math.sqrt(Math.pow(a,2) - Math.pow(y, 2));
+        
+        double x = xz * Math.cos(MaE);
+        double velX = velXZ * Math.cos(MaE);
+        
+        double z = xz * Math.sin(MaE);
+        double velZ = velXZ * Math.cos(MaE);
+        
+    }
+    
+    
+    
+    
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getZ(){
+        return z;
+    }
+    public double getSpeedX(){
+        return velX;
+    }
+    public double getSpeedY(){
+        return velY;
+    }
+    public double getSpeedZ(){
+        return velZ;
+    }
     
 }
